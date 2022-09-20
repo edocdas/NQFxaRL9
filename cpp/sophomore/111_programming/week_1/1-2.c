ee21c57c03b5b4eabcd8315812f4905306c6b9e0
Your Name <you@example.com>
2022-09-20 21:45:52 +0800

    2-1 first idea

---
+++ cpp/sophomore/111_programming/week_1/1-2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{	
+  	int num = 0;
+  	char ch;
+  	float fl = 0;
+	scanf("%d%f %c", &num, &fl, &ch);
+  	printf("%d\n%f\n%c", num, fl,ch);
+}
--- main.cpp
+++ main.cpp
@@ -1,46 +1,197 @@
-#include<cstdio>
+#include<iostream>
 
+/**
+ * A Node class of Doublely Linked List class
+ */
+template<class T>
+class Node {
+  public:
+
+  /**
+   * Construct a new Node object with no parameter
+   */
+  Node() {}
+
+  /**
+   * Construct a new Node object with parameter
+   * @param data: data to put in
+   */
+  Node(T data)
+  {
+    this->data = data;
+  }
+
+  /**
+   * Construct a new Node object with parameters
+   * @param data: data to put in
+   * @param prev: pointer to the previous node
+   * @param next: pointer to the next node
+   */
+  Node(T data, Node<T> *prev, Node<T> *next)
+  {
+    this->prev = prev;
+    this->next = next;
+    this->data = data;
+  }
+
+  //新添加的
+  void setTail(Node<T> *new_tail)
+  {
+    next = new_tail;
+  }
+
+  //新添加的
+  void setHead(Node<T> *new_head)
+  {
+    prev = new_head;
+  }
+
+  //新添加的
+  Node<T>* getNext()
+  {
+    return next;
+  }
+
+  //新添加的
+  Node<T>* getPrev()
+  {
+    return prev;
+  }
+
+  //新添加的
+  T getData()
+  {
+    return data;
+  }
+
+  private: 
+    Node<T> *prev = nullptr;
+    Node<T> *next = nullptr;
+    T data;
+};
+
+/**
+ * A Doublely Linked List class
+ */
+template<class T>
+class LinkedList {
+  public:
+
+  /**
+   * Construct a new LinkedList object with no parameter
+   */
+  LinkedList() {}
+
+  /**
+   * Add a node to the tail of the linked list
+   * @param data: data to put in
+   */
+  void addToTail(T data)
+  {
+    if(tail == nullptr)
+    {
+    tail = new Node<T>(data);
+    head = tail;
+    }
+    else {
+      Node<T> new_tail = new Node<T>(data);
+      tail->setTail(new_tail);
+    }
+  }
+
+  /**
+   * Add a node to the head of the linked list
+   * @param data: data to put in
+   */
+  void addToHead(T data)
+  {
+    if(head == nullptr)
+    {
+    head = new Node<T>(data);
+    tail = head;
+    }
+    else {
+      Node<T> new_head = new Node<T>(data);
+      head->setHead(new_head);
+    }
+  }
+
+  /**
+   * Delete a node from the linked list with the given data from the head
+   * If there are no data to be deleted, then do nothing
+   * @param data: data to delete
+   */
+  void deleteData(T data)
+  {
+    Node<T> *iter = head, *buf_del;
+    while(iter != nullptr)
+    {
+      if(iter->getData() == data)
+      {
+        iter->getPrev()->setTail(iter->getNext());
+        iter->getNext()->setHead(iter->getPrev());
+
+        if(iter == head)
+        {
+          head = iter->getNext();
+        }
+        if(iter == tail)
+        {
+          tail = iter->getPrev();
+        }
+      }
+      buf_del = iter;
+      iter = iter -> getNext();
+      delete buf_del;
+    }
+  }
+
+  /**
+   * Delete valid n nodes from the linked list with the given data from the head
+   * If there are no more data to be deleted, then just skip
+   * @param data: data to delete
+   * @param n: max number of nodes to delete
+   */
+  void deleteData(T data, int n)
+  {
+    for(int i = 0;i < n;i++)
+    {
+      deleteData(data);
+    }
+  } 
+
+  /**
+   * Overload the operator << to print out all the data in the linked list from the head
+   * There is a \n in the end of each print
+   * 
+   * Output example: 
+   * If your linked list data is 5, 4, 3, 2, 1
+   * then you should print out "(5, 4, 3, 2, 1)" with the content in quotes but without the quotes
+   */
+  friend std::ostream &operator<<(std::ostream &out, LinkedList * n)
+  {
+    Node<T> *iter = n->head;
+    int counter = 0;
+    out << "(";
+    while(iter != nullptr)
+    {
+      if(counter != 0)
+        out << ", ";
+      
+      out << iter->getData();
+      iter = iter->getNext();
+
+      counter++;
+    }
+    out << ")\n";
+  }
+
+  private: 
+    Node<T> *head = nullptr;
+    Node<T> *tail = nullptr;
+};
 
 int main()
 {
-  freopen("output.txt", "w", stdout);
-  printf("Taipei, Taiwan\n"
-        "CNN\n"
-        " -- \n" 
-        "A US congressional delegation led by Massachusetts Democratic Sen. Ed Markey has arrived in Taipei on an unannounced two-day visit, the second US congressional delegation to Taiwan this month.\n\n"
-        
-        "House Speaker Nancy Pelosi drew the ire of China when she became the first US speaker to visit in 25 years at a time when Washington-Beijing relations have been especially tense.\n\n\n"
 
-
-        "Video Ad Feedback\n"
-        "02:40 - Source: CNN\n"
-        "Beijing is mad about Pelosi's visit to Taiwan. But now some Chinese are thanking her\n"
-        "The new five-member delegation is visiting the self-governing island in an effort to \"reaffirm the United States' support for Taiwan\" and \"will encourage stability and peace across the Taiwan Strait,\" a spokesperson for Markey said in a statement.\n\n"
-
-        "The delegation includes Democratic Reps. John Garamendi, Alan Lowenthal and Don Beyer, and Republican Rep. Aumua Amata Coleman Radewagen, the statement added.\n\n"
-
-        "The group led by Markey will meet with Taiwan's President Tsai Ing-wen and Foreign Minister Joseph Wu during the visit, and will also hold discussions with the Taiwanese parliament's Foreign Affairs and National Defense committee on security and trade issues, Taiwan's Ministry of Foreign Affairs said.\n\n"
-
-        "The foreign ministry added that it sincerely welcomed the delegation, and thanked them for demonstrating the US' strong support toward Taiwan despite escalating tensions with Beijing.\n\n"
-
-        "The senator's spokesperson said the delegation \"will meet with elected leaders and members of the private sector to discuss shared interests including reducing tensions in the Taiwan Strait and expanding economic cooperation, including investments in semiconductors.\"\n\n"
-
-        "China hit back against the visit, saying it would take \"resolute countermeasures in response to the US's provocations\" in a statement Sunday from the Chinese Embassy in Washington.\n\n"
-
-        "\"Members of the US Congress should act in consistence with the US government's one-China policy,\" embassy spokesperson Liu Pengyu said on Twitter.\n\n"
-
-        "Liu said China \"firmly opposes any kind of official ties\" between the US and Taiwan, and the latest congressional delegation visit \"proves that the US does not want to see stability across the Taiwan Straits and has spared no effort to stir up confrontation between the two sides and interfere in China's internal affairs.\"\n\n"
-
-        "China's ruling Chinese Communist Party views Taiwan as part of its territory, despite having never controlled it, and has long vowed to \"reunify\" the island with the Chinese mainland, by force if necessary. Ahead of Pelosi's visit, Beijing had repeatedly warned of dire consequences should the trip go ahead - even going as far as to warn US President Joe Biden that those who played with fire would \"perish\" by it.\n\n"
-
-        "During her trip to Taiwan, Pelosi, a California Democrat, said the visit intended to make it \"unequivocally clear\" that the US would \"not abandon\" the democratically governed island.\n\n\n"
-
-        "Video Ad Feedback\n"
-        "01:11 - Source: CNN\n"
-        "Pelosi: China made a 'big fuss' of my trip\n"
-        "China responded to the speaker's trip by launching military exercises, which China's Ministry of Defense said began with drills in both the seas and airspace surrounding Taiwan. In addition to the drills, Beijing has canceled future phone calls between Chinese and US defense leaders, suspended bilateral climate talks and sanctioned Pelosi and her immediate family.\n\n"
-
-        "The White House has summoned China's ambassador to condemn the military activities and to stress the US' wish to avoid a crisis in the region. The White House has said there is no change to the US' \"One China\" policy and that Washington recognizes the People's Republic of China as the sole legitimate government of China.\n\n"
-        "The US maintains close unofficial ties with Taiwan, and is bound by law to provide Taiwan with defensive arms. But it remains deliberately vague on whether it would defend Taiwan in the event of a Chinese invasion, a policy known as \"strategic ambiguity.\"");
-  return 0;
 }
